ca5ced2c4a040810aaa39c4d71d0ab7ca15f1030
Fannichan <elekes.fanni2005@gmail.com>
2023-10-02 15:53:04 +0200

    initail

---
+++ digitalis-kepkeret-vite/main.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+let imgUrl
+let thickness
+let lenght
+
+document.addEventListener("DOMContentLoaded", init)
+function init(){
+    document.getElementById("borderButton").addEventListener("click", Border)
+}
+
+function Border(){
+    imgUrl = document.getElementById("url");
+    thickness = document.getElementById("thickness").value
+    lenght = document.getElementById("lenght").value
+    if(imgUrl == ""){
+        //hiba 1
+    }
+    else if(thickness <= 0){
+        //hiba 2
+    }
+    else if(lenght <= 0){
+        //hiba 3
+    }
+}
